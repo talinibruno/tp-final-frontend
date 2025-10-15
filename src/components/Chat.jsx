216f98b5c75c2200f8414acb1f8412a41fa4a39b
brunotalini <talinibruno@gmail.com>
2025-10-15 00:27:48 -0300

    feat: se ajustan estilos y se agregan breakpoints para vistas mobile

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -3,6 +3,7 @@ import { useChat } from "../context/ChatContext"
 import { useNavigate } from "react-router-dom"
 import { Link } from "react-router-dom"
 import NoChat from '../assets/no-chats.svg'
+import { useTheme } from "../context/ThemeContext"
 
 export default function Chat() {
   const [msg, setMsg] = useState("")
@@ -13,6 +14,8 @@ export default function Chat() {
 
   // 2. Buscamos el usuario activo
   const user = users.find(u => u.id === selectedUser)
+
+  const { theme, toggleTheme } = useTheme()
 
   const navigate = useNavigate()
 
@@ -65,8 +68,40 @@ export default function Chat() {
         showPopUp === true &&
         <section className="cont-popup">
           <div className="popup">
-            <h2>Popup de Setting</h2>
-            <button onClick={handleClick} >Cerrar</button>
+            <h2>Configuración</h2>
+            <hr className="popup-divider"></hr>
+            <h3>Tema del chat</h3>
+            <button onClick={toggleTheme} className="popup-change-theme" >
+              {
+                theme === 'light' ?
+                  (
+                    <>
+                      <span>Oscuro</span>
+                      <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-moon-stars-fill" viewBox="0 0 16 16">
+                        <path d="M6 .278a.77.77 0 0 1 .08.858 7.2 7.2 0 0 0-.878 3.46c0 4.021 3.278 7.277 7.318 7.277q.792-.001 1.533-.16a.79.79 0 0 1 .81.316.73.73 0 0 1-.031.893A8.35 8.35 0 0 1 8.344 16C3.734 16 0 12.286 0 7.71 0 4.266 2.114 1.312 5.124.06A.75.75 0 0 1 6 .278" />
+                        <path d="M10.794 3.148a.217.217 0 0 1 .412 0l.387 1.162c.173.518.579.924 1.097 1.097l1.162.387a.217.217 0 0 1 0 .412l-1.162.387a1.73 1.73 0 0 0-1.097 1.097l-.387 1.162a.217.217 0 0 1-.412 0l-.387-1.162A1.73 1.73 0 0 0 9.31 6.593l-1.162-.387a.217.217 0 0 1 0-.412l1.162-.387a1.73 1.73 0 0 0 1.097-1.097zM13.863.099a.145.145 0 0 1 .274 0l.258.774c.115.346.386.617.732.732l.774.258a.145.145 0 0 1 0 .274l-.774.258a1.16 1.16 0 0 0-.732.732l-.258.774a.145.145 0 0 1-.274 0l-.258-.774a1.16 1.16 0 0 0-.732-.732l-.774-.258a.145.145 0 0 1 0-.274l.774-.258c.346-.115.617-.386.732-.732z" />
+                      </svg>
+                    </>
+                  ) :
+                  (
+                    <>
+                      <span>Claro</span>
+                      <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-brightness-high-fill" viewBox="0 0 16 16">
+                        <path d="M12 8a4 4 0 1 1-8 0 4 4 0 0 1 8 0M8 0a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-1 0v-2A.5.5 0 0 1 8 0m0 13a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-1 0v-2A.5.5 0 0 1 8 13m8-5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1 0-1h2a.5.5 0 0 1 .5.5M3 8a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1 0-1h2A.5.5 0 0 1 3 8m10.657-5.657a.5.5 0 0 1 0 .707l-1.414 1.415a.5.5 0 1 1-.707-.708l1.414-1.414a.5.5 0 0 1 .707 0m-9.193 9.193a.5.5 0 0 1 0 .707L3.05 13.657a.5.5 0 0 1-.707-.707l1.414-1.414a.5.5 0 0 1 .707 0m9.193 2.121a.5.5 0 0 1-.707 0l-1.414-1.414a.5.5 0 0 1 .707-.707l1.414 1.414a.5.5 0 0 1 0 .707M4.464 4.465a.5.5 0 0 1-.707 0L2.343 3.05a.5.5 0 1 1 .707-.707l1.414 1.414a.5.5 0 0 1 0 .708" />
+                      </svg>
+                    </>
+                  )
+              }
+            </button>
+            <hr className="popup-divider"></hr>
+            <h3>Cambiar Apodo</h3>
+            <p className="popup-coming-soon">¡Próximamente!</p>
+            <hr className="popup-divider"></hr>
+            <h3>Cambiar Fondo</h3>
+            <p className="popup-coming-soon">¡Próximamente!</p>
+            <hr className="popup-divider"></hr>
+
+            <button className="popup-close" onClick={handleClick} >Cerrar</button>
           </div>
         </section>
       }
@@ -80,29 +115,31 @@ export default function Chat() {
                 alt={user.name}
                 className="chat-avatar"
               />
-              <strong>{user.name}</strong>
-              {user.lastSeen !== "" && <span className="last-seen">Last seen: {user.lastSeen}</span>}
+              <div className="chat-user-data">
+                <strong>{user.name}</strong>
+                {user.lastSeen !== "" && <span className="last-seen">Last seen: {user.lastSeen}</span>}
+              </div>
             </div>
           </div>
 
           <div className="chat-actions">
-            <button title="Camera">
+            <button title="Camera" className="btn-chat-camera" >
               <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-camera-fill" viewBox="0 0 16 16">
                 <path d="M10.5 8.5a2.5 2.5 0 1 1-5 0 2.5 2.5 0 0 1 5 0" />
                 <path d="M2 4a2 2 0 0 0-2 2v6a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V6a2 2 0 0 0-2-2h-1.172a2 2 0 0 1-1.414-.586l-.828-.828A2 2 0 0 0 9.172 2H6.828a2 2 0 0 0-1.414.586l-.828.828A2 2 0 0 1 3.172 4zm.5 2a.5.5 0 1 1 0-1 .5.5 0 0 1 0 1m9 2.5a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0" />
               </svg>
             </button>
-            <button title="Gallery">
+            <button title="Gallery" className="btn-chat-gallery">
               <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-image-fill" viewBox="0 0 16 16">
                 <path d="M.002 3a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v10a2 2 0 0 1-2 2h-12a2 2 0 0 1-2-2zm1 9v1a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V9.5l-3.777-1.947a.5.5 0 0 0-.577.093l-3.71 3.71-2.66-1.772a.5.5 0 0 0-.63.062zm5-6.5a1.5 1.5 0 1 0-3 0 1.5 1.5 0 0 0 3 0" />
               </svg>
             </button>
-            <button onClick={handleClick} title="Settings">
+            <button onClick={handleClick} title="Settings" className="btn-chat-settings">
               <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-gear-fill" viewBox="0 0 16 16">
                 <path d="M9.405 1.05c-.413-1.4-2.397-1.4-2.81 0l-.1.34a1.464 1.464 0 0 1-2.105.872l-.31-.17c-1.283-.698-2.686.705-1.987 1.987l.169.311c.446.82.023 1.841-.872 2.105l-.34.1c-1.4.413-1.4 2.397 0 2.81l.34.1a1.464 1.464 0 0 1 .872 2.105l-.17.31c-.698 1.283.705 2.686 1.987 1.987l.311-.169a1.464 1.464 0 0 1 2.105.872l.1.34c.413 1.4 2.397 1.4 2.81 0l.1-.34a1.464 1.464 0 0 1 2.105-.872l.31.17c1.283.698 2.686-.705 1.987-1.987l-.169-.311a1.464 1.464 0 0 1 .872-2.105l.34-.1c1.4-.413 1.4-2.397 0-2.81l-.34-.1a1.464 1.464 0 0 1-.872-2.105l.17-.31c.698-1.283-.705-2.686-1.987-1.987l-.311.169a1.464 1.464 0 0 1-2.105-.872zM8 10.93a2.929 2.929 0 1 1 0-5.86 2.929 2.929 0 0 1 0 5.858z" />
               </svg>
             </button>
-            <button onClick={() => { navigate("/help") }} title="Help">
+            <button onClick={() => { navigate("/help") }} title="Help" className="btn-chat-help">
               <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-question-circle-fill" viewBox="0 0 16 16">
                 <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0M5.496 6.033h.825c.138 0 .248-.113.266-.25.09-.656.54-1.134 1.342-1.134.686 0 1.314.343 1.314 1.168 0 .635-.374.927-.965 1.371-.673.489-1.206 1.06-1.168 1.987l.003.217a.25.25 0 0 0 .25.246h.811a.25.25 0 0 0 .25-.25v-.105c0-.718.273-.927 1.01-1.486.609-.463 1.244-.977 1.244-2.056 0-1.511-1.276-2.241-2.673-2.241-1.267 0-2.655.59-2.75 2.286a.237.237 0 0 0 .241.247m2.325 6.443c.61 0 1.029-.394 1.029-.927 0-.552-.42-.94-1.029-.94-.584 0-1.009.388-1.009.94 0 .533.425.927 1.01.927z" />
               </svg>
--- src/views/Help.jsx
+++ src/views/Help.jsx
@@ -1,7 +1,112 @@
+import React, { useState } from "react"
+import { useNavigate } from "react-router-dom"
+import { useTheme } from "../context/ThemeContext"
+import LoginScreen from '../assets/images/LoginScreen.png'
+import ViteLogo from '../assets/dev-logos/vite.svg'
+import VercelLogo from '../assets/dev-logos/vercel.svg'
+import ReactLogo from '../assets/dev-logos/react.svg'
+import AppLogo from '../assets/images/logo-new.png'
+
 const Help = () => {
+
+  const { theme, toggleTheme } = useTheme()
+  const navigate = useNavigate()
+
   return (
-    <main>
-      <h1>Hola a todos, HELP</h1>
+    <main className="help-page">
+      <div className="help-page-div">
+
+        <div className="btns-help-container">
+          <button onClick={() => { navigate("/chat") }} className="help-btns">
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-left-circle" viewBox="0 0 16 16">
+              <path fill-rule="evenodd" d="M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8m15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0m-4.5-.5a.5.5 0 0 1 0 1H5.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L5.707 7.5z" />
+            </svg>
+            <span>Volver al Chat</span>
+          </button>
+
+          <button onClick={toggleTheme} className="help-btns" >
+            {
+              theme === 'light' ?
+                (
+                  <>
+                    <span>Oscuro</span>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-moon-stars-fill" viewBox="0 0 16 16">
+                      <path d="M6 .278a.77.77 0 0 1 .08.858 7.2 7.2 0 0 0-.878 3.46c0 4.021 3.278 7.277 7.318 7.277q.792-.001 1.533-.16a.79.79 0 0 1 .81.316.73.73 0 0 1-.031.893A8.35 8.35 0 0 1 8.344 16C3.734 16 0 12.286 0 7.71 0 4.266 2.114 1.312 5.124.06A.75.75 0 0 1 6 .278" />
+                      <path d="M10.794 3.148a.217.217 0 0 1 .412 0l.387 1.162c.173.518.579.924 1.097 1.097l1.162.387a.217.217 0 0 1 0 .412l-1.162.387a1.73 1.73 0 0 0-1.097 1.097l-.387 1.162a.217.217 0 0 1-.412 0l-.387-1.162A1.73 1.73 0 0 0 9.31 6.593l-1.162-.387a.217.217 0 0 1 0-.412l1.162-.387a1.73 1.73 0 0 0 1.097-1.097zM13.863.099a.145.145 0 0 1 .274 0l.258.774c.115.346.386.617.732.732l.774.258a.145.145 0 0 1 0 .274l-.774.258a1.16 1.16 0 0 0-.732.732l-.258.774a.145.145 0 0 1-.274 0l-.258-.774a1.16 1.16 0 0 0-.732-.732l-.774-.258a.145.145 0 0 1 0-.274l.774-.258c.346-.115.617-.386.732-.732z" />
+                    </svg>
+                  </>
+                ) :
+                (
+                  <>
+                    <span>Claro</span>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-brightness-high-fill" viewBox="0 0 16 16">
+                      <path d="M12 8a4 4 0 1 1-8 0 4 4 0 0 1 8 0M8 0a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-1 0v-2A.5.5 0 0 1 8 0m0 13a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-1 0v-2A.5.5 0 0 1 8 13m8-5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1 0-1h2a.5.5 0 0 1 .5.5M3 8a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1 0-1h2A.5.5 0 0 1 3 8m10.657-5.657a.5.5 0 0 1 0 .707l-1.414 1.415a.5.5 0 1 1-.707-.708l1.414-1.414a.5.5 0 0 1 .707 0m-9.193 9.193a.5.5 0 0 1 0 .707L3.05 13.657a.5.5 0 0 1-.707-.707l1.414-1.414a.5.5 0 0 1 .707 0m9.193 2.121a.5.5 0 0 1-.707 0l-1.414-1.414a.5.5 0 0 1 .707-.707l1.414 1.414a.5.5 0 0 1 0 .707M4.464 4.465a.5.5 0 0 1-.707 0L2.343 3.05a.5.5 0 1 1 .707-.707l1.414 1.414a.5.5 0 0 1 0 .708" />
+                    </svg>
+                  </>
+                )
+            }
+          </button>
+        </div>
+
+        <div className="help-header">
+          <h1>¿Necesitas ayuda?</h1>
+          <p>Esta es una aplicación de mensajería que permite comunicarte fácilmente con tus contactos a través de mensajes de chat.</p>
+          <p>Actualmente se encuentra disponible para computadoras y web entrando desde cualquier navegador.</p>
+          <img className="help-login-screen" src={LoginScreen} alt="Foto del Login de la aplicación" />
+        </div>
+
+        <hr />
+
+        <h2>Funcionalidades y características principales</h2>
+        <div className="help-features-general">
+
+          <div className="help-feat">
+            <h3>Estructura y secciones</h3>
+            <p>En la pantalla principal luego de loguearte, podrás ver a tu izquierda, la barra lateral o "Sidebar" donde se encuentran todos tus contactos.</p>
+            <p>Por el lado derecho está la pantalla del chat, donde verás los mensajes guardados con la persona seleccionada y podrás enviar nuevos mensajes si así lo deseas!</p>
+          </div>
+
+          <div className="help-feat">
+            <h3>Privacidad y seguridad</h3>
+            <p>Cifrado de extremo a extremo: todos los mensajes están protegidos para que solo tú y la persona con la que hablas puedan leerlos.</p>
+          </div>
+
+          <div className="help-feat">
+            <h3>Ajustes y personalización</h3>
+            <p>Cambia el tema seleccionado, en la aplicación podrás optar por utilizar un tema claroo un tema oscuro</p>
+          </div>
+        </div>
+
+        <hr />
+
+        <h2>¡Próximamente! Nuevas Funcionalidades</h2>
+        <p>¿Cuáles son las nuevas funcinoalidades para nuestra próxima versión?</p>
+
+        <div className="help-new-features">
+          <p>Se podrán agregar apodos a tus contactos, para que los puedas reconocer más fácilmente</p>
+          <p>Habilitaremos la opción para cambiar el fondo de tu chat y que lo personalices a tu gusto</p>
+        </div>
+
+        <hr />
+
+        <div className="help-dev-tech" >
+          <h2>Tecnologías utilizadas</h2>
+          <p>Nuestra plataforma está desarrollada con Vite y React, lo que permite un entorno rápido y moderno para el desarrollo web, y está desplegada en Vercel, que garantiza un alojamiento ágil, seguro y escalable.</p>
+
+          <div className="help-dev-logos">
+            <img src={ViteLogo} alt="Logo de Vite" className="logo-vite" />
+            <img src={ReactLogo} alt="Logo de React" className="logo-react" />
+            <img src={VercelLogo} alt="Logo de Vercel" className="logo-vercel" />
+          </div>
+        </div>
+
+        <hr />
+
+        <div className="help-sign">
+          <h4>Desarrollado por Bruno Talini.</h4>
+          <p>2025. UTN - Diplomatura Full Stack. (Frontend)</p>
+        </div>
+      </div>
     </main>
   )
 }
